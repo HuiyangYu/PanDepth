915bcaac91bb9b9e2899bbbe2b88918928d9a9ab
Zou Lab <41780741+HuiyangYu@users.noreply.github.com>
2024-01-01 08:30:23 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ INCLUDE_DIR=$(pwd)/include
 if [ ! -d "$BIN_DIR" ]; then
   	mkdir "$BIN_DIR"
 fi
-/usr/bin/g++ -L$LIB_DIR -Wl,-rpath=$LIB_DIR --std=c++11 -g -O2 $SRC_DIR/PanDepth.cpp -lhts -lpthread -lz -ldeflate -I $INCLUDE_DIR -o $BIN_DIR/pandepth
+/usr/bin/g++ -L$LIB_DIR -Wl,-rpath=$LIB_DIR --std=c++11 -g -O3 $SRC_DIR/PanDepth.cpp -lhts -lpthread -lz -ldeflate -I $INCLUDE_DIR -o $BIN_DIR/pandepth
 if [ $? -ne 0 ]; then
   echo "Installation of PanDepth has failed."
 else
